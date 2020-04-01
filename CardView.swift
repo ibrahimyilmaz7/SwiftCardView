b1986d1025c788c5f93d5a2509a5d44b5b733c85
IBRAHIM YILMAZ <0ibrahimyilmaz@gmail.com>
2020-04-01 16:54:24 +0300

    Update CardView.swift

--- CardView.swift
+++ CardView.swift
@@ -9,12 +9,12 @@ import UIKit
 @IBDesignable
 class CardView: UIView {
     
-    @IBInspectable var cornerRadius: CGFloat = 2
+    @IBInspectable var cornerRadius: CGFloat = 10
     
-    @IBInspectable var shadowOffsetWidth: Int = 0
-    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowOffsetWidth: Int = 5
+    @IBInspectable var shadowOffsetHeight: Int = 5
     @IBInspectable var shadowColor: UIColor? = .black
-    @IBInspectable var shadowOpacity: Float = 0.5
+    @IBInspectable var shadowOpacity: Float = 0.7
     
     override func layoutSubviews() {
         layer.cornerRadius = cornerRadius
